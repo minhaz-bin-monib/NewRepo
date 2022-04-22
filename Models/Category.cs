461cb78f57d6ed9e1bee9a38d61e6ed83e34a31b
Minhaz <67540595+minhaz-bin-monib@users.noreply.github.com>
2022-04-22 20:14:12 +0600

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBooksWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+
+        [System.ComponentModel.DisplayName("Name")]
+        public string name { get; set; }
+
+        [System.ComponentModel.DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
