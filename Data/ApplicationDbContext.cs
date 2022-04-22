ee5922969139a509360c4718792dfc0693ea7eab
Minhaz <67540595+minhaz-bin-monib@users.noreply.github.com>
2022-04-23 00:14:17 +0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBooksWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
